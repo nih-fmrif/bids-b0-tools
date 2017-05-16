f676cf3ed1f8b721c2097c2f4fd4d58078f107aa
Jerry French <jerryfrenchjr@gmail.com>
2017-05-16 13:42:23 -0400

    The compareMI.py script now takes a list of csv files given from the command line to build a pandas dataframe and ultimately plot the data. It requires the MI csv file from the noCorr output from distortionFix.py to calculate the percent change in MI. It will gather the median, mean, and standard deviation of this percent change in MI and plot it on the x-axis along with values for each session using a bar chart.

--- compareMI.py
+++ compareMI.py
@@ -1,75 +1,58 @@
 #!/usr/bin/env python
 
-import time, sys, os
-import csv
+import sys, os
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 
-# create dataframes from MI csv files
-nc = pd.read_csv('noCorr_MI.csv', names=['subs', 'MI_nc'])
-ae = pd.read_csv('afniBlip_MI.csv', names=['subs', 'MI_ae'])
-ab = pd.read_csv('afniB0_MI.csv', names=['subs', 'MI_ab'])
-fe = pd.read_csv('fslBlip_MI.csv', names=['subs', 'MI_fe'])
-fb = pd.read_csv('fslB0_MI.csv', names=['subs', 'MI_fb'])
 
-# merge all MI columns into a single dataframe
-df_1 = nc.merge(ae, on='subs')
-df_2 = df_1.merge(ab, on='subs')
-df_3 = df_2.merge(fe, on='subs')
-df_m = df_3.merge(fb, on='subs')
+# create dataframes from command line argument list of csv files
+# each file should have 'sub' as first column header and the
+# 'corrMethod' (from distortionFix.py) as the second column header
 
-# sort new dataframe alphabetically
-df_ms = df_m.sort('subs')
-df_ms['pcMI_ae'] = (df_ms['MI_ae']-df_ms['MI_nc'])*100/(df_ms['MI_nc']) 
-df_ms['pcMI_ab'] = (df_ms['MI_ab']-df_ms['MI_nc'])*100/(df_ms['MI_nc'])
-df_ms['pcMI_fe'] = (df_ms['MI_fe']-df_ms['MI_nc'])*100/(df_ms['MI_nc'])
-df_ms['pcMI_fb'] = (df_ms['MI_fb']-df_ms['MI_nc'])*100/(df_ms['MI_nc'])
-# print df_ms
+print "file list is " + str(sys.argv[1:])
 
-df_median = pd.DataFrame({'subs': ['median'],
-                          'pcMI_ae': [df_ms['pcMI_ae'].median(axis='index')],
-                          'pcMI_ab': [df_ms['pcMI_ab'].median(axis='index')],
-                          'pcMI_fe': [df_ms['pcMI_fe'].median(axis='index')],
-                          'pcMI_fb': [df_ms['pcMI_fb'].median(axis='index')]})
+# create dataframe
+df = pd.concat([pd.read_csv(f, index_col='sub') for f in sys.argv[1:]], axis=1, join='outer')
 
-df_mean   = pd.DataFrame({'subs': ['mean'],
-                          'pcMI_ae': [df_ms['pcMI_ae'].mean(axis='index')],
-                          'pcMI_ab': [df_ms['pcMI_ab'].mean(axis='index')],
-                          'pcMI_fe': [df_ms['pcMI_fe'].mean(axis='index')],
-                          'pcMI_fb': [df_ms['pcMI_fb'].mean(axis='index')]})
+# remove all rows with NaN in nc, ae, and fe
+# df = df[np.isfinite(df['nc']+df['ae']+df['fe'])].sort()
+df = df[np.isfinite(df['nc']+df['fby'])].sort()
 
-df_stdev  = pd.DataFrame({'subs': ['stdev'],
-                          'pcMI_ae': [df_ms['pcMI_ae'].std(axis='index')],
-                          'pcMI_ab': [df_ms['pcMI_ab'].std(axis='index')],
-                          'pcMI_fe': [df_ms['pcMI_fe'].std(axis='index')],
-                          'pcMI_fb': [df_ms['pcMI_fb'].std(axis='index')]})
+# create new columns for percent changes from noCorr
+for corr in [corrMeth for corrMeth in df.columns if corrMeth != 'nc']:
+   df[corr] = (df[corr]-df['nc'])*100/df['nc']
 
-df_final = df_ms.append([df_median, df_mean, df_stdev])
-print df_final
+corrMeans = {}
+corrMeds = {}
+corrStd = {}
 
+for corr in df.columns:
+   corrMeans.update ({corr : df[corr].mean()})
+   corrMeds.update  ({corr : df[corr].median()})
+   corrStd.update   ({corr : df[corr].std()})
+
+df2 = pd.Series(corrMeans.values(), index=corrMeans.keys(), name='mean')
+df3 = pd.Series(corrMeds.values(),  index=corrMeds.keys(),  name='median')
+df4 = pd.Series(corrStd.values(),   index=corrStd.keys(),   name='std')
+df = df.append(df2)
+df = df.append(df3)
+df = df.append(df4)
+
+print df
+print df.info()
+# print df.describe()
 
 # plot the MI values for each distortion correction technique by subject
-df_final[['MI_nc', 'MI_ae', 'MI_ab', 'MI_fe', 'MI_fb']].plot(kind='bar', grid=False)
+df[df.columns].plot(kind='bar', grid=False)
 plt.title("Mutual Information for each Correction Technique by Subject")
 plt.xlabel('Subjects')
 plt.ylabel('Mutual Information')
-plt.legend(['MI_nc', 'MI_ae', 'MI_ab', 'MI_fe', 'MI_fb'],
-           labels=['noCorr', 'afniBlip', 'afniB0', 'fslBlip', 'fslB0'],
-	   bbox_to_anchor=(1.05, 1), loc=2, fontsize='small', borderaxespad=0.)
-# plt.ylim(0.14, 0.35)
+# plt.legend(['MI_nc', 'MI_ae', 'MI_ab', 'MI_fe', 'MI_fb'],
+#            labels=['noCorr', 'afniBlip', 'afniB0', 'fslBlip', 'fslB0'],
+#            bbox_to_anchor=(1.05, 1), loc=2, fontsize='small', borderaxespad=0.)
+plt.ylim(-5, 5)
 plt.savefig("test1.png", bbox_inches='tight')
-# os.system("display test1.png")
+os.system("display test1.png")
 
 
-# plot the percent change in MI values for each distortion correction
-# technique by subject
-df_final[['pcMI_ae', 'pcMI_ab', 'pcMI_fe', 'pcMI_fb']].plot(kind='bar', grid=False)
-plt.title("Mutual Information for each Correction Technique by Subject")
-plt.xlabel('Subjects')
-plt.ylabel('Percent Change in Mutual Information')
-plt.legend(['pcMI_ae', 'pcMI_ab', 'pcMI_fe', 'pcMI_fb'],
-           labels=['afniBlip', 'afniB0', 'fslBlip', 'fslB0'],
-	   bbox_to_anchor=(1.05, 1), loc=2, fontsize='small', borderaxespad=0.)
-plt.ylim(-15, 25)
-plt.savefig("test2.png", bbox_inches='tight')
-os.system("display test2.png")
